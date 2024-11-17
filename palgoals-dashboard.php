b86da5defffe2da399551b1bbf6c9633a4405fc4
palgooal <palgooal@gmail.com>
2024-11-17 23:57:45 +0200

    up

---
+++ palgoals-dashboard.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: Palgoals Dashboard
+Plugin URI: https://palgoals.com
+Description: Palgoals Dashboard plugin with multi-language support
+Version: 1.0
+Author: hazem alyahya
+Author URI: https://palgoals.com
+License: GPLv2 or later
+Text Domain: palgoals-dash
+Domain Path: /languages
+*/
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+// تحميل ملفات الترجمة للإضافة
+function palgoals_load_textdomain() {
+    load_plugin_textdomain('palgoals-dash', false, dirname(plugin_basename(__FILE__)) . '/languages');
+}
+add_action('plugins_loaded', 'palgoals_load_textdomain');
+require_once plugin_dir_path(__FILE__) . 'includes/login-dashboard.php';
+require_once plugin_dir_path(__FILE__) . 'includes/index-dashboard.php';
+require_once plugin_dir_path(__FILE__) . 'includes/pages-dashboard.php';
+//require_once plugin_dir_path(__FILE__) . 'includes/blog.php';
+require_once plugin_dir_path(__FILE__) . 'includes/pg-food-menus/menus-dashboard.php';
+require_once plugin_dir_path(__FILE__) . 'includes/pg-food-menus/category-menus-dashboard.php';
+require_once plugin_dir_path(__FILE__) . 'includes/media.php';
+require_once plugin_dir_path(__FILE__) . 'includes/enqueue-scripts.php';
+
+
+add_filter('show_admin_bar', '__return_false');
+
+
+// عند تفعيل الإضافة
+function palgoals_activate_plugin() {
+    palgoals_dashboard_rewrite_rule();
+    palgoals_pages_dashboard_rewrite_rule();
+    flush_rewrite_rules(); // تحديث قواعد إعادة الكتابة
+}
+register_activation_hook(__FILE__, 'palgoals_activate_plugin');
+
+// عند إلغاء تفعيل الإضافة
+function palgoals_deactivate_plugin() {
+    flush_rewrite_rules(); // إعادة القواعد لحالتها الافتراضية
+}
+register_deactivation_hook(__FILE__, 'palgoals_deactivate_plugin');
+
+// تحديث الروابط الدائمة عند إعادة تفعيل الإضافة
+add_action('admin_init', function() {
+    if (is_plugin_active('palgoals-dashboard/palgoals-dashboard.php')) {
+        flush_rewrite_rules();
+    }
+});
+
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './admin/**/*.{html,js,php}', // تحديد ملفات لوحة التحكم
+  ],
+  theme: {
+    extend: {
+      // تخصيص خصائص Tailwind مثل الألوان والخطوط
+      colors: {
+        dashboardBg: '#f5f5f5', // لون خلفية مخصص للوحة التحكم
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      cardCustom: {
+        'position': 'relative',
+        'margin-bottom': '1.5rem',
+        'border-radius': '0.75rem',
+        'border-width': '1px',
+        '--tw-border-opacity': '1',
+        'border-color': 'rgb(231 234 238 / var(--tw-border-opacity))',
+        '--tw-bg-opacity': '1',
+        'background-color': 'rgb(255 255 255 / var(--tw-bg-opacity))',
+      },
+      
+    },
+  },
+  plugins: [
+   
+  ],
+};
+
+
